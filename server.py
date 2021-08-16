60de107e252f1985dcf51174067d5d7c31ae2e84
peqltta <peqlkush@protonmail.com>
2021-08-16 18:37:34 +0100

    first commit

---
+++ server.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, request, jsonify, send_from_directory
+from aiml import Kernel
+import os
+
+import speech_recognition as sr
+import pyttsx3
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.probability import FreqDist
+import nltk
+nltk.download("punkt")
+nltk.download("vader_lexicon")
+sid = SentimentIntensityAnalyzer()
+app = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates')
+
+kernel = Kernel()
+
+if os.path.isfile("bot_brain.brn"):
+	os.remove("bot_brain.brn")
+	kernel.bootstrap(learnFiles = os.path.abspath("aiml/std-startup.xml"), commands = "load aiml b")
+	kernel.saveBrain("bot_brain.brn")
+else:
+	kernel.bootstrap(learnFiles = os.path.abspath("aiml/std-startup.xml"), commands = "load aiml b")
+	kernel.saveBrain("bot_brain.brn")
+
+def speaks(audioString):
+	engine = pyttsx3.init()
+	engine.setProperty('rate', 180)
+	engine.say(audioString)
+	engine.runAndWait()
+	
+def recordAudio():
+	# Record Audio
+	r = sr.Recognizer()
+	with sr.Microphone() as source:
+		print("Say something!")
+		audio = r.listen(source)
+ 
+	# Speech recognition using Google Speech Recognition
+	data = ""
+	voicecommand = ""
+	try:
+		voicecommand = r.recognize_google(audio)
+		print("You said: " + voicecommand)
+	except sr.UnknownValueError:
+		print("Google Speech Recognition could not understand audio")
+	except sr.RequestError as e:
+		print("Could not request results from Google Speech Recognition service; {0}".format(e))
+	print('Sentiment Score (For later use - NLP)')
+	print(sid.polarity_scores(voicecommand))
+	return voicecommand
+	
+def jarvis(data):
+	s = kernel.respond(data)
+	return s
+
+@app.route("/voice", methods=['POST'])
+def voice(): 
+	data = request.data.decode()
+	print(data)
+	jarvis(data)
+	answer = kernel.respond(data)
+	return answer #jsonify({'status':'OK','answer':jarvis(data)})
+
+@app.route("/")
+def hello():
+	return render_template('chat.html')
+
+@app.route("/ask", methods=['POST'])
+def ask():
+	message = str(request.form['messageText'])
+	# kernel now ready for use
+	while True:
+		if message == "quit":
+			exit()
+		elif message == "save":
+			kernel.saveBrain("bot_brain.brn")
+		else:
+			bot_response = kernel.respond(message)
+			# print bot_response
+			return jsonify({'status':'OK','answer':bot_response})
+
+if __name__ == "__main__":
+	app.run(debug=True)
+	app.run(host='127.0.0.1', port=5000)
---
+++ static/script.js
@@ -0,0 +1,47 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+var grammar = '#JSGF V1.0; grammar = ' + (' | ') + ' ;'
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+recognition.continuous = false;
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+var diagnostic = document.querySelector('.output');
+var bg = document.querySelector('html');
+var hints = document.querySelector('.hints');
+recognition.onresult = function(event) {
+    var voicecommand = event.results[0][0].transcript;
+    $(".media-list").append('<li class="media"><div class="media-body"><div class="media"><div //class="media-body">' + voicecommand + '<hr/></div></div></div></li>');
+    var http = new XMLHttpRequest();
+    var url = '/voice';
+    var params = voicecommand;
+	var xhr = new XMLHttpRequest(); //make xhr
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == XMLHttpRequest.DONE) {
+			answer = xhr.responseText;
+			$(".media-list").append('<li class="media"><div class="media-body"><div class="media"><div //class="media-body">' + answer + '<hr/></div></div></div></li>');
+			document.querySelector('button.hidden').click();
+			speechSynthesis.speak(new SpeechSynthesisUtterance(answer));
+		}
+	}
+	xhr.open('POST', url, true);
+	xhr.setRequestHeader('Content-Type', 'x-www-form-urlencoded');
+	xhr.send(voicecommand);
+    console.log(voicecommand)
+    console.log('Confidence: ' + event.results[0][0].confidence);
+}
+recognition.onspeechend = function() {
+    recognition.stop();
+}
+
+recognition.addEventListener('end', function() {
+    setTimeout(() => { recognition.start(); }, 3500);
+});
+recognition.onnomatch = function(event) {
+    diagnostic.textContent = 'huh';
+}
+recognition.start();
